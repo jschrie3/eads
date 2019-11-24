35dc03d4b691845a60e9404b843b91e1d5cf74da
Christopher Prener <chris.prener@slu.edu>
2019-11-24 10:22:27 -0600

    create levels and further consolidate

---
+++ inst/extdata/stl_levels.R
@@ -0,0 +1,50 @@
+# levels
+
+# dependencies
+library(dplyr)
+library(sf)
+library(tigris)
+
+# list count names
+mo_names <- c("Franklin", "Jefferson", "Lincoln", "St. Charles", "St. Louis", "Warren")
+il_names <- c("Calhoun", "Clinton", "Greene", "Jersey", "Macoupin", "Madison", "Monroe", "Randolph", "St. Clair")
+
+# download missouri counties
+mo_counties <- counties(state = "MO", class = "sf") %>%
+  filter(NAME %in% mo_names) %>%
+  mutate(STATE = "MO") %>%
+  select(STATE, STATEFP, COUNTYFP, GEOID, NAME, NAMELSAD)
+
+# download illinois counties
+il_counties <- counties(state = "IL", class = "sf") %>%
+  filter(NAME %in% il_names) %>%
+  mutate(STATE = "IL") %>%
+  select(STATE, STATEFP, COUNTYFP, GEOID, NAME, NAMELSAD)
+
+# combine and remove geometry
+stl_levels <- rbind(mo_counties, il_counties)
+st_geometry(stl_levels) <- NULL
+
+# clean-up
+rm(il_names, il_counties, mo_names, mo_counties)
+
+# define levels
+stl_levels %>%
+  rename(
+    state = STATE,
+    state_fips = STATEFP,
+    county_fips = COUNTYFP,
+    geoid = GEOID,
+    name = NAME,
+    name_full = NAMELSAD
+  ) %>%
+  mutate(
+    city = ifelse(geoid == "29510", TRUE, FALSE),
+    city_county = ifelse(geoid %in% c("29189", "29510"), TRUE, FALSE),
+    core = ifelse(geoid %in% c("29189", "29510", "17119", "17163"), TRUE, FALSE),
+    ew_gateway = ifelse(geoid %in% c("29189", "29510", "29071", "29099", "29183",
+                                     "17119", "17163", "17133"), TRUE, FALSE)
+  ) -> stl_levels
+
+# save data
+save(stl_levels, file = "data/stl_levels.rda")
