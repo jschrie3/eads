70461723791e18603f14f4043fd7ed091324e8d4
Christopher Prener <chris.prener@slu.edu>
2019-11-24 10:32:51 -0600

    clean-up levels object

--- inst/extdata/stl_levels.R
+++ inst/extdata/stl_levels.R
@@ -13,13 +13,13 @@ il_names <- c("Calhoun", "Clinton", "Greene", "Jersey", "Macoupin", "Madison", "
 mo_counties <- counties(state = "MO", class = "sf") %>%
   filter(NAME %in% mo_names) %>%
   mutate(STATE = "MO") %>%
-  select(STATE, STATEFP, COUNTYFP, GEOID, NAME, NAMELSAD)
+  select(STATE, GEOID, NAMELSAD)
 
 # download illinois counties
 il_counties <- counties(state = "IL", class = "sf") %>%
   filter(NAME %in% il_names) %>%
   mutate(STATE = "IL") %>%
-  select(STATE, STATEFP, COUNTYFP, GEOID, NAME, NAMELSAD)
+  select(STATE, GEOID, NAMELSAD)
 
 # combine and remove geometry
 stl_levels <- rbind(mo_counties, il_counties)
@@ -32,19 +32,24 @@ rm(il_names, il_counties, mo_names, mo_counties)
 stl_levels %>%
   rename(
     state = STATE,
-    state_fips = STATEFP,
-    county_fips = COUNTYFP,
     geoid = GEOID,
-    name = NAME,
-    name_full = NAMELSAD
+    name = NAMELSAD
   ) %>%
+  mutate(name = ifelse(geoid == "29510", "St. Louis City", name)) %>%
   mutate(
     city = ifelse(geoid == "29510", TRUE, FALSE),
     city_county = ifelse(geoid %in% c("29189", "29510"), TRUE, FALSE),
     core = ifelse(geoid %in% c("29189", "29510", "17119", "17163"), TRUE, FALSE),
     ew_gateway = ifelse(geoid %in% c("29189", "29510", "29071", "29099", "29183",
-                                     "17119", "17163", "17133"), TRUE, FALSE)
-  ) -> stl_levels
+                                     "17119", "17163", "17133"), TRUE, FALSE),
+    metro_east = ifelse(state == "IL", TRUE, FALSE),
+    metro_west = ifelse(state == "MO", TRUE, FALSE),
+    msa = TRUE
+  ) %>%
+  select(-state) %>%
+  arrange(as.numeric(geoid)) -> stl_levels
 
 # save data
 save(stl_levels, file = "data/stl_levels.rda")
+
+# readr::write_csv(stl_levels, path = "stl_levels.csv")
